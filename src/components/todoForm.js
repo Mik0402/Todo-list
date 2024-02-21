9ada77eee5a258e2294a6eb3ed93a2872e4e4bb0
Mikaela Lammers <miklammers12@gmail.com>
2024-02-20 20:39:21 -0600

    Creating project frame work

---
+++ src/components/todoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function todoForm {
+    return (
+        <form className="todoForm">
+            <input type="text" className="todo-input" placeholder="What is the task today?" />
+            <button type="submit" className="todo-btn"></button>
+        </form>
+    )
+}
